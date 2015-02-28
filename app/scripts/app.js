621175d43c754572fbd2aacfe7f27f231c8453d6
Brian Sipple <Bsipple57@gmail.com>
2015-02-27 18:58:21 -0600

    initial commit. Basic app setup

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+(function (document) {
+  'use strict';
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  var app = document.querySelector('#app');
+  app.appName = 'Google DevFest Twin Cities';
+
+
+
+  app.devfestStartTime = new Date(2015, 2, 21, 7);
+
+  // Listen for template bound event to know when bindings
+  // have resolved and content has been stamped to the page
+  app.addEventListener('template-bound', function() {
+    console.log('Our app is ready to rock!');
+  });
+
+// wrap document so it plays nice with other libraries
+// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+})(wrap(document));
